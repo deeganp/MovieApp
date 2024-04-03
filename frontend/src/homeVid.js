87a3f62b72a8f1d5c99dba9e7046079f9303605c
deeganp <deeganp.motro@gmail.com>
2024-04-03 16:11:55 -0400

    updated home video to autoplay and be more seamless on mobile

--- frontend/src/homeVid.js
+++ frontend/src/homeVid.js
@@ -4,11 +4,12 @@ import './homeVid.css';
 const HomeVid = () => {
   return (
     <div className="video-container">
-      <video controls className="video" height="360" src="./images/HomeVid.mp4" type="video/mp4">
+      <video autoPlay muted loop playsInline className="video" height="360" src="./images/HomeVid.mp4" type="video/mp4">
         Your browser does not support the video tag.
       </video>
     </div>
   );
 };
 
-export default HomeVid; 
+export default HomeVid;
+
